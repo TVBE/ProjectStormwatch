b1f187756a1c8ccddde4a99177421e3125308bbe
Tim Verberne <timverbe@gmail.com>
2023-03-31 21:51:06 +0200

    90326 - added door interface

---
+++ Source/Frostbite/Interfaces/Public/DoorInterface.h
@@ -0,0 +1,42 @@
+// Copyright (c) 2022-present Barrelhouse
+// Written by Tim Verberne
+// This source code is part of the project Frostbite
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DoorInterface.generated.h"
+
+UENUM(BlueprintType)
+enum class EDoorState : uint8
+{
+    Open        UMETA(DisplayName = "Open"), 
+    Closed      UMETA(DisplayName = "Closed"),
+    Opening     UMETA(DisplayName = "Opening"),
+    Closing     UMETA(DisplayName = "Closing"),
+    Locked      UMETA(DisplayName = "Locked"),
+};
+
+UINTERFACE(Blueprintable, Meta = (DisplayName = "Door Interface"))
+class UDoor : public UInterface
+{
+    GENERATED_BODY()
+};
+
+class IDoor
+{
+    GENERATED_BODY()
+
+public:
+    /** Attempts to open the door. */
+    UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "Door", Meta = (DisplayName = "Open"))
+    void Open();
+
+    /** Attempts to close the door. */
+    UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "Door", Meta = (DisplayName = "Close"))
+    void Close();
+    
+    /** Returns the door's state. */
+    UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "Door", Meta = (DisplayName = "Get Door State"))
+    EDoorState GetDoorState();
+};
