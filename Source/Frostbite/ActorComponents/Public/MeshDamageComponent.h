0581fcfbc3ba4575162e3677708cfd027aab7bb0
Tim Verberne <timverbe@gmail.com>
2023-05-01 11:09:46 +0200

    89786 - added mesh damage component

---
+++ Source/Frostbite/ActorComponents/Public/MeshDamageComponent.h
@@ -0,0 +1,78 @@
+// Copyright (c) 2022-present Barrelhouse
+// Written by Tim Verberne
+// This source code is part of the project Frostbite
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "MeshDamageComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnDamagePercentageChangedDelegate, const float, NewPercentage);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnDamageThresholdReachedDelegate);
+
+UCLASS(NotBlueprintable, BlueprintType, ClassGroup = "Physics", Meta = (BlueprintSpawnableComponent, Displayname = "Damage Component"))
+class UMeshDamageComponent : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:
+	/** Delegate that is called when the damage percentage of the component is changed. */
+	UPROPERTY(BlueprintAssignable, Category = "Damage|Delegates", Meta = (DisplayName = "On Damaged"))
+	FOnDamagePercentageChangedDelegate OnDamaged;
+
+	/** Delegate that is called when the damage threshold of the component is reached. */
+	UPROPERTY(BlueprintAssignable, Category = "Damage|Delegates", Meta = (DisplayName = "On Damage Threshold Reached"))
+	FOnDamageThresholdReachedDelegate OnDamageThresholdReached;
+
+	/** The damage threshold for the component to broadcast it's OnDamageThresholdReached delegate. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Damage", Meta = (DisplayName = "Threshold", Units = "Newtons"))
+	float Threshold {100000.0f};
+
+	/** The minimum impact force required to increment the damage. */
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Damage", Meta = (DisplayName = "Minimum Required Force", Units = "Newtons"))
+	float ImpulseForceThreshold {500.0f};
+
+private:
+	/** The current damage level. */
+	float DamageLevel {0.0f};
+
+	/** The current damage percentage. */
+	float DamagePercentage {0.0f};
+
+	bool IsThresholdReached {false};
+
+	bool IsCooldownActive {false};
+	
+	UPROPERTY()
+	UStaticMeshComponent* MeshComponent;
+
+	FTimerHandle CooldownTimerHandle;
+	
+public:	
+	UMeshDamageComponent();
+
+	/** Resets the damage component. */
+	UFUNCTION(BlueprintCallable, Category = "Damage", Meta = (DisplayName = "Reset Damage"))
+	void ResetDamage();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+private:
+	UFUNCTION()
+	void OnStaticMeshComponentHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+	void AddDamage(const float Value);
+	
+	void SetDamageLevel(const float Value);
+
+	void UpdateDamagePercentage();
+
+	void StartCooldown();
+
+	UFUNCTION()
+	void HandleCooldownFinished();
+	
+};
