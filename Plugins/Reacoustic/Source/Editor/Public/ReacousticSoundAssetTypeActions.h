fc1756df50e4959595181678a05950b92b6f0f32
Nino Saglia <nino.saglia97@gmail.com>
2023-09-06 10:18:38 +0200

    minor cleanup

--- Plugins/Reacoustic/Source/Editor/Public/ReacousticSoundAssetTypeActions.h
+++ Plugins/Reacoustic/Source/Editor/Public/ReacousticSoundAssetTypeActions.h
@@ -23,7 +23,6 @@ public:
 	EVisibility GetThumbnailOverlayVisibility() const;
 	virtual TSharedPtr<SWidget> GetThumbnailOverlay(const FAssetData& AssetData) const override;
 	FReply OnThumbnailOverlayClicked() const;
-	FReply OnDragStared() const;
 	
 	 
 
