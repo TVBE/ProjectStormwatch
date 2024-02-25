770be71ebd995c888157b8e80dcd233565eabbe4
TVBE <timverbe@gmail.com>
2024-02-25 11:54:53 +0100

    [SW-89764] Added semantic component.

---
+++ Source/Stormwatch/Components/Private/BHSemanticComponent.cpp
@@ -0,0 +1,35 @@
+﻿// Copyright (c) 2022-present Barrelhouse. All rights reserved.
+
+#include "BHSemanticComponent.h"
+
+UBHSemanticComponent::UBHSemanticComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+bool UBHSemanticComponent::IsDisplayable() const
+{
+	return !SemanticName.IsEmpty() && IsValid(Thumbnail);
+}
+
+const FText& UBHSemanticComponent::GetSemanticName() const
+{
+	return SemanticName;
+}
+
+const FText& UBHSemanticComponent::GetSemanticDescription() const
+{
+	return SemanticDescription;
+}
+
+const UTexture2D* UBHSemanticComponent::GetThumbnail() const
+{
+	return Thumbnail;
+}
+
+const TArray<FBHSemanticGameplayTag>& UBHSemanticComponent::GetSemanticTags() const
+{
+	return SemanticTags;
+}
+
+
---
+++ Source/Stormwatch/Components/Public/BHSemanticComponent.h
@@ -0,0 +1,54 @@
+// Copyright (c) 2022-present Barrelhouse. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "BHSemanticComponent.generated.h"
+
+USTRUCT(BlueprintType)
+struct FBHSemanticGameplayTag
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere)
+	FText TagName;
+};
+
+UCLASS(BlueprintType, ClassGroup = "Barrelhouse", Meta = (BlueprintSpawnableComponent))
+class STORMWATCH_API UBHSemanticComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UBHSemanticComponent();
+
+	UFUNCTION(BlueprintPure, Category = "BHSemanticComponent")
+	bool IsDisplayable() const;
+
+	UFUNCTION(BlueprintGetter)
+	const FText& GetSemanticName() const;
+
+	UFUNCTION(BlueprintGetter)
+	const FText& GetSemanticDescription() const;
+
+	UFUNCTION(BlueprintGetter)
+	const UTexture2D* GetThumbnail() const;
+
+	UFUNCTION(BlueprintGetter)
+	const TArray<FBHSemanticGameplayTag>& GetSemanticTags() const;
+	
+private:
+	UPROPERTY(EditDefaultsOnly, BlueprintGetter = GetSemanticName, Category = "Semantics", Meta = (DisplayName = "Name"))
+	FText SemanticName;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintGetter = GetSemanticName, Category = "Semantics",
+	Meta = (Multiline = "true", DisplayName = "Description"))
+	FText SemanticDescription;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintGetter = GetThumbnail, Category = "Semantics")
+	UTexture2D* Thumbnail = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintGetter = GetSemanticTags, Category = "Tags")
+	TArray<FBHSemanticGameplayTag> SemanticTags;
+};
